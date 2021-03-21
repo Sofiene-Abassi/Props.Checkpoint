f5c097762e985b863791bae90d1f6e647c940937
Sofiene-Abassi <sofiene-456@hotmail.frgit remote add origin https://github.com/Sofiene-Abassi/first_repogit push origin mastergit config --global user.email sofiene-456@hotmail.fr>
2021-03-21 22:16:24 +0100

    first commit

---
+++ src/profile/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from "prop-types";
+
+function Profile(props) {
+    return (
+        <div>
+            <h1 style={{fontFamily:"Papyrus"}}>My name is {props.FullName} </h1>
+            <h2 style={{textAlign:"center"}}>{props.Bio}</h2>
+            <h3 style={{color:"blue",fontFamily:"Lucida Handwriting"}}>He is also a {props.Profession}</h3>
+            {props.children}
+
+        </div>
+    )
+  
+    
+}
+
+
+
+Profile.propTypes = {
+    FullName : PropTypes.string,
+    Bio : PropTypes.string,
+    Profession : PropTypes.string,
+};
+Profile.defaultProps = {
+    FullName: "The Batman",
+    Bio: "Super Hero",
+    Profession: "Detective and Crime Fighter",
+
+   };
+
+   export default Profile;
